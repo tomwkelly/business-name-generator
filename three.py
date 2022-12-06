8c6146db3699ff7d7a57e522e04ae68223ef4ecb
Tom Kelly <tom-kelly@hotmail.co.uk>
2022-12-06 12:54:01 +0000

    updated results

--- three.py
+++ three.py
@@ -1,9 +1,10 @@
 import string
 import random
 import socket
-import subprocess
 import sys
 import asyncio
+import whois
+import json
 
 DOMAIN_URL='https://domains.google.com/registrar/search?searchTerm='
 
@@ -23,25 +24,20 @@ if len(sys.argv) > 3:
     elif x in consonants:
       consonants.remove(x)
 
-
-async def run_whois(domain):
-    command = "whois {} | grep -e 'No match for domain' -e 'Domain not found'".format(domain)
-    proc = await asyncio.create_subprocess_shell(
-        command,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
-
-    await proc.communicate()
-    
-    if proc.returncode == 0:
-      print(domain, f'{DOMAIN_URL}{domain}')
+async def new_whois(domain):
+  try:
+    w = whois.whois(domain)
+    if 'No match for' in w.text or 'Domain not found' in w.text:
+        return f'{domain}, {DOMAIN_URL}{domain}'
+  except:
+    return None
 
       
 async def run_all_whois(items):
   t = []
   for i in items:
-    t.append(run_whois(i))
-  await asyncio.gather(*t)
+    t.append(new_whois(i))
+  return await asyncio.gather(*t)
 
 
 def add_consonants(items):
@@ -71,16 +67,16 @@ def add_items(items, type):
   
   raise ValueError(f'{t} is not a valid type')
 
-def get_combos():
-  if interp[0] not in ['v','c']:
-    raise ValueError(f'{interp[0]} is not a valid type')
+def get_combos(i):
+  if i[0] not in ['v','c']:
+    raise ValueError(f'{i[0]} is not a valid type')
   
   if start != '':
     items = [start]
-    return add_items(items, interp)
+    return add_items(items, i)
 
-  items = consonants if interp[0] == 'c' else vowels
-  return add_items(items, interp[1:])
+  items = consonants if i[0] == 'c' else vowels
+  return add_items(items, i[1:])
 
 
 
@@ -92,11 +88,12 @@ def get_vowel():
 
 def get_ran_all():
   f = []
-  for c in get_combos():
-    for e in endings:
-      x = c + e
-      if(check_available(x)):
-        f.append(x)
+  for i in interp.split(','):
+    for c in get_combos(i):
+      for e in endings:
+        x = c + e
+        if(check_available(x)):
+          f.append(x)
   print('Sock complete, checking:', f)
   return f
 
@@ -110,9 +107,17 @@ def check_available(domain):
 
 def double_check():
   a = get_ran_all()
-  asyncio.run(run_all_whois(a))
+  x = asyncio.run(run_all_whois(a))
+  for i in x:
+    if(i is not None):
+      print(i)
 
-      
+# f = open('cvc.json')
+# words = json.load(f)
 
+# x = asyncio.run(run_all_whois(words))
+# for i in x:
+#   if(i is not None):
+#     print(i)
 
 double_check()
